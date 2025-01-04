095a91517ecbda6895bea07576fc9f60105ec695
Mostafa Abdelmeged <43843461+Mostafamaag@users.noreply.github.com>
2025-01-04 19:24:47 +0200

    Update firebase.js

--- Frontend/src/app/firebase/firebase.js
+++ Frontend/src/app/firebase/firebase.js
@@ -1,15 +1,14 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp, getApps } from "firebase/app";
 import { getAuth } from "firebase/auth"
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDUVoJHRR6KQ2AYxkZODUq_cSoKsDam3fY",
-    authDomain: "mobile-auth-31540.firebaseapp.com",
-    projectId: "mobile-auth-31540",
-    storageBucket: "mobile-auth-31540.firebasestorage.app",
-    messagingSenderId: "643718857201",
-    appId: "1:643718857201:web:42f54d4aa91e4d60e08450",
-    measurementId: "G-BBFWRSYKYW"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
 };
 
 // Initialize Firebase
